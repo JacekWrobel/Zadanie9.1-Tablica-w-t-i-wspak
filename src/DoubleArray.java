f9ec018e99ca95abd87ac631e37c38874c5b45e1
Jacek <ja77co@wp.pl>
2019-05-29 20:21:23 +0200

    Zadanie "do połowy" bez lustrzanego odbicia

---
+++ src/DoubleArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class DoubleArray {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] arrayStart = new int[10];
+        for (int i = 0; i < arrayStart.length; i++) {
+            arrayStart[i] = rand.nextInt(100);
+
+        }
+        String arrayInString = Arrays.toString(arrayStart);
+        System.out.println(arrayInString);
+
+        for (int i = 0, j= arrayStart.length -1; i <  j; i++, j--) {
+            System.out.print(arrayStart[i]+" ");
+            System.out.println(arrayStart[j] +" ");
+            }
+        }
+/*
+        for (int i = arrayStart.length-1; i >=0 ; i--) {
+            System.out.print(arrayStart[i] + " ");
+        }
+
+ */
+        /*
+        Następnie w drugiej pętli wyświetl na ekranie zawartość tablicy oraz jej lustrzane odbicie.
+        Do wyświetlania możesz wykorzystać tylko jedną pętlę i tą pętlą powinna być pętla for.
+        Nie możesz w programie tworzyć żadnych dodatkowych tablic.
+         */
+    }
+}
